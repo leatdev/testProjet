ce22f74b982453f4e8850ef263c24f80c159d190
mohamet aminendiaye <leatdev@github.com>
2023-11-13 20:48:39 -0500

    second commit

--- app/src/main/java/com/example/locslspecies/MainActivity.kt
+++ app/src/main/java/com/example/locslspecies/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Test push to github")
+                    Greeting("Test again push to github")
                 }
             }
         }
